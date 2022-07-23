8703d391e4f02cd19260ae1c2dd564cc653da5be
Iftakhar Husan <iftakharn19@gmail.com>
2022-07-23 18:32:22 +0600

    Add UniqueConstraint and move ProjectBeneficiaries to project app

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-from django.conf import settings
 from django.utils import timezone
+
+from beneficiaries.models import Beneficiaries
 
 
 class Projects(models.Model):
     title = models.CharField(_("Title"), max_length=255, blank=False)
     description = models.TextField(_("Description"), )
     eligibility = models.TextField(_("Beneficiary eligibility"))
-    banner_image = models.ImageField(height_field=settings.IMAGE_SIZES['banner'].height, width_field=settings.IMAGE_SIZES['banner'].width, blank=True)
+    banner_image = models.ImageField(blank=True, null=True)
 
     total_budget = models.DecimalField(max_digits=24, decimal_places=4)
     total_budget = models.DecimalField(max_digits=24, decimal_places=4)
@@ -19,7 +20,30 @@ class Projects(models.Model):
 
 
 class ProjectCoordinators(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=[
+                                    'project', 'coordinator'], name="%(app_label)s_%(class)s__check_project_and_coordinator_is_unique_together")
+        ]
+
     project = models.ForeignKey(Projects, on_delete=models.CASCADE)
     coordinator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    
-    assign_date_time = models.DateTimeField(auto_now_add=True, default=timezone.now)
+
+    assign_date_time = models.DateTimeField(
+        auto_now_add=True, default=timezone.now)
+
+
+class ProjectBeneficiaries(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=[
+                                    'project', 'beneficiary'], name="%(app_label)s_%(class)s__check_project_and_beneficiary_is_unique_together")
+        ]
+
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    beneficiary = models.ForeignKey(Beneficiaries, on_delete=models.CASCADE)
+    benefactor = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    benefit_receive_date_time = models.DateTimeField(null=True, blank=True)
+    beneficiary_join_date_time = models.DateTimeField(
+        auto_now_add=True, default=timezone.now)
