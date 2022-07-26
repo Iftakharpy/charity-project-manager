42623f03c7c7fd6d9b3a4b4633ddad9541143edc
Iftakhar Husan <iftakharn19@gmail.com>
2022-07-27 01:35:43 +0600

    Add django-rest-framework routed urls in url conf

--- backend/charity_project_manager_core/urls.py
+++ backend/charity_project_manager_core/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from rest_framework import routers
+
+from users.views import UserRetrieveAndUpdateAPI
+
+router = routers.DefaultRouter()
+router.register('users', UserRetrieveAndUpdateAPI)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('api/users/', include('users.urls')),
+    path('api/', include(router.urls)),
+    path('api/', include('rest_framework.urls')), # This line should stay at the end of the list
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
