8eb4ac84604c236e9dbf80857ffb093ce2a94474
Iftakhar Husan <iftakharn19@gmail.com>
2022-07-30 00:28:36 +0600

    Register models in admin and add dunder method str

--- backend/beneficiaries/admin.py
+++ backend/beneficiaries/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Beneficiaries, BeneficiaryAddress, District, Thana, PostCode, Ward, Village
+
+
+admin.site.register(Beneficiaries)
+admin.site.register(BeneficiaryAddress)
+admin.site.register(District)
+admin.site.register(Thana)
+admin.site.register(PostCode)
+admin.site.register(Ward)
+admin.site.register(Village)
--- backend/beneficiaries/models.py
+++ backend/beneficiaries/models.py
@@ -4,48 +4,83 @@ from django.utils import timezone
 
 
 class Beneficiaries(models.Model):
+    class Meta:
+        verbose_name = 'Beneficiary'
+        verbose_name_plural = 'Beneficiaries'
+
     first_name = models.CharField(max_length=75)
     last_name = models.CharField(max_length=75)
     date_of_birth = models.DateField()
     phone_number = models.CharField(max_length=20)
 
-    profile_picture = models.ImageField(_("Profile Picture"), blank=True, null=True)
+    profile_picture = models.ImageField(
+        _("Profile Picture"), blank=True, null=True)
     description = models.TextField(blank=True)
 
-    last_updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    last_updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Name: {self.first_name} {self.last_name} Phone Number: {self.phone_number}"
 
 
 class District(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class Thana(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
 
+    def __str__(self) -> str:
+        return f"{self.name}"
 
-class PostOffice(models.Model):
+
+class PostCode(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
+
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class Ward(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class Village(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
 
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class BeneficiaryAddress(models.Model):
+    class Meta:
+        verbose_name = 'Beneficiary Address'
+        verbose_name_plural = 'Beneficiary Addresses'
+
     beneficiary = models.ForeignKey(Beneficiaries, on_delete=models.CASCADE)
-    
-    district = models.ForeignKey(District, on_delete=models.CASCADE, null=True, blank=True)
-    thana = models.ForeignKey(Thana, on_delete=models.CASCADE, null=True, blank=True)
-    post_office = models.ForeignKey(PostOffice, on_delete=models.CASCADE, null=True, blank=True)
-    ward = models.ForeignKey(Ward, on_delete=models.CASCADE, null=True, blank=True)
-    Village = models.ForeignKey(Village, on_delete=models.CASCADE, null=True, blank=True)
+
+    district = models.ForeignKey(
+        District, on_delete=models.CASCADE, null=True, blank=True)
+    thana = models.ForeignKey(
+        Thana, on_delete=models.CASCADE, null=True, blank=True)
+    post_office = models.ForeignKey(
+        PostCode, on_delete=models.CASCADE, null=True, blank=True)
+    ward = models.ForeignKey(
+        Ward, on_delete=models.CASCADE, null=True, blank=True)
+    village = models.ForeignKey(
+        Village, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.beneficiary} - District: {self.district} Thana: {self.thana}"
