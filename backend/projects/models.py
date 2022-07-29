a8796869774a1d2b08e03b821a1abcf4e09bb193
Iftakhar Husan <iftakharn19@gmail.com>
2022-07-30 00:22:03 +0600

    Add dunder method str and verbose names

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -7,6 +7,10 @@ from beneficiaries.models import Beneficiaries
 
 
 class Projects(models.Model):
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+    
     title = models.CharField(_("Title"), max_length=255, blank=False)
     description = models.TextField(_("Description"), )
     eligibility = models.TextField(_("Beneficiary eligibility"))
@@ -18,9 +22,14 @@ class Projects(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+    def __str__(self) -> str:
+        return f"{self.title} - {self.total_budget}"
+
 
 class ProjectCoordinators(models.Model):
     class Meta:
+        verbose_name = 'Project Coordinator'
+        verbose_name_plural = 'Project Coordinators'
         constraints = [
             models.UniqueConstraint(fields=[
                                     'project', 'coordinator'], name="%(app_label)s_%(class)s__check_project_and_coordinator_is_unique_together")
@@ -30,11 +39,16 @@ class ProjectCoordinators(models.Model):
     coordinator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     assign_date_time = models.DateTimeField(
-        auto_now_add=True, default=timezone.now)
+        auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"Coordinator: {self.coordinator} Project: {self.project}"
 
 
 class ProjectBeneficiaries(models.Model):
     class Meta:
+        verbose_name = 'Project Beneficiary'
+        verbose_name_plural = 'Project Beneficiaries'
         constraints = [
             models.UniqueConstraint(fields=[
                                     'project', 'beneficiary'], name="%(app_label)s_%(class)s__check_project_and_beneficiary_is_unique_together")
@@ -46,4 +60,7 @@ class ProjectBeneficiaries(models.Model):
 
     benefit_receive_date_time = models.DateTimeField(null=True, blank=True)
     beneficiary_join_date_time = models.DateTimeField(
-        auto_now_add=True, default=timezone.now)
+        auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"Beneficiary: {self.beneficiary} Project: {self.project}"
