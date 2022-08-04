92b8882dd1fe4d6b407bff0fa3287f2e534944bb
Iftakhar Husan <iftakharn19@gmail.com>
2022-08-05 02:16:34 +0600

    Allow requests from all hosts and setup cross site creedentials

--- backend/charity_project_manager_core/settings/dev.py
+++ backend/charity_project_manager_core/settings/dev.py
@@ -2,7 +2,16 @@ from .base import *
 from .env import *
 
 
-DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# insert corsheaders app before rest_framework
+INSTALLED_APPS.insert(INSTALLED_APPS.index('rest_framework'), 'corsheaders')
+
+# insert CorsMiddleware before django CommonMiddleware
+MIDDLEWARE.insert(MIDDLEWARE.index('django.middleware.common.CommonMiddleware'), 'corsheaders.middleware.CorsMiddleware')
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 TIME_ZONE = "Asia/Dhaka"
 # TIME_ZONE = 'Europe/London'
