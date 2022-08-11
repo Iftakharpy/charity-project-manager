26e01db2b1235f4924a3c6f41641f8c31f7d8478
Iftakhar Husan <iftakharn19@gmail.com>
2022-08-12 01:15:21 +0600

    Add tailwind plugins forms, line-clamp, aspect-ratio and typography

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
+  safelist: [
+    {pattern: /Toastify*/},
+  ],
   content: [
     "./src/**/*.{html,js,jsx,tsx}",
     "./index.html",
@@ -10,5 +14,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
